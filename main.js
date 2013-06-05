b2ef9fd2c5718fb621b08e25184a09feb49eda7a
zarrow <zarrow567@hotmail.com>
2013-06-05 15:37:17 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+var app = { 
+
+findByName:
+  function() {
+		console.log('findByName');
+
+		this.store.findByName($('.search-key').val(), 
+		function(employees) 
+		{ 
+var l = employees.length;
+
+		  var e;
+
+		  $('.employee-list').empty();
+		  for (var i=0; i<l; i++) {
+			e = employees[i];
+			$('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+
+		  }
+		});
+	},
+	initialize: function() {
+		this.store = new MemoryStore();
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
+
+,
+
+	showAlert: Function(message, title) {
+		if(navigator.notification){
+			navigator.notiification.alert(message, null, title, 'OK');
+		}
+		else {
+			alert(title ? (title+": "+message) : message);
+		}
+	}
+};
+
+
+app.initialize(MemoryStore);
+
+initialize: function() {
+	var self = this;
+	this.store = new MemoryStor(function() {
+		self.alert('Store Initialized', 'Info');
+	});
+	$('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
