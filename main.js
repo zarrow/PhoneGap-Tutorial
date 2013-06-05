1e41b6fa8e908f426be309fd952a122ce09a2fd2
zarrow <zarrow567@hotmail.com>
2013-06-05 16:02:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
 var app = { 
 
-findByName:
-	function() {
-		console.log('findByName');
-
-		this.store.findByName($('.search-key').val(), 
-		function(employees) 
-		{ 
-var l = employees.length;
-
-		  var e;
-
-		  $('.employee-list').empty();
-		  for (var i=0; i<l; i++) {
-			e = employees[i];
-			$('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-
-		  }
+	findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
 		});
 	},
+
 	initialize: function() {
 		var self = this;
 		this.store = new MemoryStore(function() {
 			self.renderHomeView();
 		});
-	}
-
-,
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+	},
 
 	showAlert: function(message, title) {
 		if(navigator.notification){
@@ -38,14 +26,9 @@ var l = employees.length;
 	},
 
 	renderHomeView: function() {
-		var html = "<div class='header'><h1>Home</h1></div>" +
-			"<div class='search-view'>" +
-          	  	"<input class='search-key'/>" +
-       			"<ul class='employee-list'></ul>" +
-   		        "</div>"
-			$('body').html(html);
-			$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	}
+  		$('body').html(this.homeTpl());
+    		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
 };
 
 
