23be12bdb5500a0954a1e41542bb062198ab73c8
zarrow <zarrow567@hotmail.com>
2013-06-05 15:49:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -26,13 +26,23 @@ var l = employees.length;
 
 ,
 
-	showAlert: Function(message, title) {
+	showAlert: function(message, title) {
 		if(navigator.notification){
 			navigator.notiification.alert(message, null, title, 'OK');
 		}
 		else {
 			alert(title ? (title+": "+message) : message);
 		}
+	},
+
+	renderHomeView: function() {
+		var html = "<div class='header'><h1>Home</h1></div>" +
+			"<div class='search-view'>" +
+          	  	"<input class='search-key'/>" +
+       			"<ul class='employee-list'></ul>" +
+   		        "</div>"
+			$('body').html(html);
+			$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	}
 };
 
